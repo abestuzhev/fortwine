21f6385d6710ead540db5eec58b3f65d09df8fa6
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2017-12-08 16:11:09 +0300

    changes

--- script-header.js
+++ script-header.js
@@ -7,4 +7,35 @@ $(document).ready(function(){
    .mouseout(function() {
      $(this).find('img').attr('src', './local/templates/b2c/img/ico_in-dark.png');
    });
+
+
+    $('.slider-detail').slick({
+        autoplay: false,
+        dots: true,
+        arrows: false,
+        infinite: true,
+        speed: 500,
+        adaptiveHeight: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+
+
+    /*соглашение на обработку персональных данных*/
+    function DisabledFormButton(form, check, btn_form){
+        $("form input[type='checkbox']").on('change', function(){
+            if($(check).prop('checked')){
+                $(btn_form).removeAttr('disabled');
+            }else {
+                $(btn_form).attr('disabled', 'disabled');
+            }
+        });
+    }
+
+    DisabledFormButton('#registration-form','#registration__consent-data','.registration-form__btn');
+
+
+    $('.js-phone-mask').mask('+7(000)000-00-00');
+    $('.js-data-mask').mask('00.00.0000');
+
 });
